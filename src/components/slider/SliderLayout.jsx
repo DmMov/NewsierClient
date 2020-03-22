9350e95c6ce325a06bbf1b6d9ec5b7f3e5496667
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-03-22 22:12:46 +0200

    fix slider animation

--- src/components/slider/SliderLayout.jsx
+++ src/components/slider/SliderLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { object, number, array, func } from 'prop-types';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 // * Components
 import { Slide } from './Slide';
@@ -11,16 +11,17 @@ import './SliderLayout.scss';
 
 export const SliderLayout = ({ slide, index, dots, jumpTo }) =>
   <div id="slider">
-      <TransitionGroup className="slide-wrap">
-        <CSSTransition
-          in={true}
-          key={index}
-          timeout={1000}
-          classNames="fade"
-        >
-          <Slide slide={slide} index={index} />
-        </CSSTransition>
-      </TransitionGroup>
+    <TransitionGroup className="slide-wrap">
+      <CSSTransition
+        key={slide.id}
+        in={true}
+        appear={true}
+        timeout={1000}
+        classNames="fade"
+      >
+        <Slide slide={slide} index={index} />
+      </CSSTransition>
+    </TransitionGroup>
       <Dots
         dots={dots}
         jumpTo={jumpTo}
