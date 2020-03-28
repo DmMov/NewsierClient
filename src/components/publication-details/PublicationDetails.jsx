11843b35311cd14c4a5e812c0cac0609a192c935
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-03-28 16:39:29 +0200

    upgrade the publication details component

--- src/components/publication-details/PublicationDetails.jsx
+++ src/components/publication-details/PublicationDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiberManualRecordRounded, ChevronRight, Visibility } from '@material-ui/icons';
+import { FiberManualRecordRounded, Visibility } from '@material-ui/icons';
 
 // * Components
 import { Link } from 'components';
@@ -7,27 +7,31 @@ import { Link } from 'components';
 // * Sass
 import './PublicationDetails.scss';
 
-export const PublicationDetails = ({ id, category, categoryId, title, publisher, views }) =>
+export const PublicationDetails = ({ id, category, categoryId, title, publisher, publisherId, views }) =>
   <div className="publicationDetails">
     <Link
       to={`/publications/by-category/${categoryId}`}
     >
-      <span className="publicationDetails__category">{ category }</span>
+      <span className="publicationDetails__category">{category}</span>
     </Link>
     <Link
       to={`/publication/${id}`}
     >
-      <h4 className="publicationDetails__title">{ title }</h4>
+      <h4 className="publicationDetails__title">{title}</h4>
     </Link>
     <div className="publicationDetails__relatedInfo">
       <p className="publisher">
         <span className="publisher__prefix">автор, </span>
-        <span className="publisher__name">{ publisher }</span>
+        <Link
+          to={`/publications/by-publisher/${publisherId}`}
+        >
+          <span className="publisher__name">{publisher}</span>
+        </Link>
       </p>
-      <FiberManualRecordRounded className="dotIcon"/>
+      <FiberManualRecordRounded className="dotIcon" />
       <p className="views">
-        <Visibility className="statIcon"/>
-        <span className="views__count">{ views }</span>
+        <Visibility className="statIcon" />
+        <span className="views__count">{views}</span>
       </p>
     </div>
   </div>
