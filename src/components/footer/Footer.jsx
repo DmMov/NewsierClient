5f059df03e77ec2100421e566d676324f7206da0
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-05-24 18:28:56 +0300

    Refactoring

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,30 +11,33 @@ import {
 // * Sass
 import './Footer.scss';
 
+const AuthCheckedLinks = ({ authorized }) => authorized ?
+  <Link to="/cabinet" classes={['btn']}>кабінет</Link> :
+  <>
+    <Link to="/sign-in" classes={['btn']}>вхід</Link>
+    <FiberManualRecord className="dotIcon" />
+    <Link to="/sign-up" classes={['btn']}>реєстрація</Link>
+  </>;
+
 export const Footer = ({ authorized }) =>
   <footer className="newsierFooter">
     <Logo color="white" />
+    <p className="newsierFooter__sponsor">
+      <span className="preText">спонсор, </span>
+      <a href="https://ritm.tv/" className="link">ritm.tv</a>
+    </p>
     <nav className="newsierFooter__menu">
       <Link to="/" classes={['btn']}>головна</Link>
       <FiberManualRecord className="dotIcon" />
-      {!authorized && <Link to="/sign-in" classes={['btn']}>вхід</Link>}
-      {!authorized && <FiberManualRecord className="dotIcon" />}
-      {!authorized && <Link to="/sign-up" classes={['btn']}>реєстрація</Link>}
-      {authorized && <Link to="/cabinet" classes={['btn']}>кабінет</Link>}
+      <AuthCheckedLinks authorized={authorized} />
     </nav>
-    <p className="newsierFooter__sponsor">
-      <span className="preText">спонсор, </span>
-      <a href="https://ritm.tv/" className="link">
-        ritm.tv
-      </a>
-    </p>
     <p className="newsierFooter__copyright">
       <Copyright className="icon" /> Copyright
       <span className="digit year">{new Date().getFullYear()}. </span>
       <span className="newsier">newsier</span>.
       <span className="allRights">Всі права захищені.</span>
     </p>
-  </footer>
+  </footer>;
 
 Footer.propTypes = {
   authorized: bool.isRequired
