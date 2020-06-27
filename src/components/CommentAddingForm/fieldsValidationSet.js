65ef836cd77e75d8caff9eb118b41c58dd93bfa6
DmMov <dmmov.live@gmail.com>
2020-06-27 21:31:27 +0300

    Fixed an issue with the comment adding form validation

--- src/components/CommentAddingForm/fieldsValidationSet.js
+++ src/components/CommentAddingForm/fieldsValidationSet.js
@@ -1,9 +1,10 @@
 // * Utils
-import { required, minLength } from 'utils/validators';
+import { required, minLength, maxLength } from 'utils/validators';
 
 export const fieldsValidationSet = {
   value: [
     [required, 'enter your comment.'],
-    [minLength(5), 'comment must contain at least 5 characters']
+    [minLength(5), 'comment must contain at least 5 characters'],
+    [maxLength(256), 'comment cannot contain more than 256 characters'],
   ]
 };
